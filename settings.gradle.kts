124d932198faff00acb7f12599156ae7496095a8
SRanK1 <kevin.alfaro@unmsm.edu.pe>
2024-04-12 03:02:42 -0500

    Commit Inicial

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "19200211_20200077_HappyBirthday"
+include(":app")
+ 
